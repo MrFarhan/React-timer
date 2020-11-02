497b27f4e114a959eeffbd7e28ce42867ad9851f
MrFarhan <muhammadfarhan02345@gmail.com>
2020-11-02 19:43:27 +0500

    initial commit

---
+++ src/Timer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Timer = () => {
+    let D = new Date;
+
+
+    return (
+        <div>
+        {console.log(D, "D")}
+              
+        </div>
+    )
+}
